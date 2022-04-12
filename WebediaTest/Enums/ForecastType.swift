ffa2e3746e08e1719aedf68073f7e208733cc8a9
Reqven <manuu.marchand@gmail.com>
2022-04-12 03:19:21 +0200

    MasterViewController refactor

---
+++ WebediaTest/Enums/ForecastType.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastType.swift
+//  WebediaTest
+//
+//  Created by Manu on 12/04/2022.
+//
+
+import Foundation
+
+enum ForecastType: Int {
+  case upcoming
+  case hottest
+}
--- WebediaTest/MasterViewController.swift
+++ WebediaTest/MasterViewController.swift
@@ -4,25 +4,49 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController, ImageDownloadDelegate {
+class MasterViewController: UITableViewController {
   
+  //MARK: - Properties
   @IBOutlet weak var sortingControl: UISegmentedControl!
-  
   var detailViewController: DetailViewController? = nil
-  var forecast = Forecast()
   
+  //MARK: DataSource
+  var forecast = Forecast()
+  var upcoming: Forecast {
+    forecast.sorted { $0.day < $1.day }
+  }
+  var hottest: Forecast {
+    forecast
+      .filter { $0.chanceRain < 0.5 }
+      .sorted { $0.high > $1.high }
+  }
+  
+  //MARK: DataSourceType
+  private var dataSourceIndex: Int {
+    sortingControl.selectedSegmentIndex
+  }
+  private var dataSourceType: ForecastType? {
+    ForecastType(rawValue: dataSourceIndex)
+  }
+  private var dataSource: Forecast {
+    guard let type = dataSourceType else { return [] }
+    switch(type) {
+      case .upcoming: return upcoming
+      case .hottest: return hottest
+    }
+  }
+  
+  //MARK: - Methods
   override func viewDidLoad() {
     super.viewDidLoad()
     loadForecast()
-  
-    
     
     if let split = splitViewController {
       let controllers = split.viewControllers
       detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
     }
     
-    sortingControl.addTarget(self, action: #selector(self.sortingControlAction), for: .valueChanged)
+    sortingControl.addTarget(self, action: #selector(self.onSort), for: .valueChanged)
   }
   
   private func loadForecast() {
@@ -40,45 +64,54 @@ class MasterViewController: UITableViewController, ImageDownloadDelegate {
     }
   }
   
-  func imageDownloaded(for forecast: Day) {
-    //TODO: Update models and reloadData
-  }
-  
-  @objc func sortingControlAction(_ segmentedControl: UISegmentedControl) {
-    //TODO: Sort days and reloadData
+  @objc func onSort(_ segmentedControl: UISegmentedControl) {
+    tableView.reloadData()
   }
   
   // MARK: - Segues
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showDetail" {
       if let indexPath = tableView.indexPathForSelectedRow {
-        let forecast = forecast[indexPath.row]
+        let day = dataSource[indexPath.row]
         let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
         controller.delegate = self
-        controller.day = forecast
+        controller.day = day
         controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
         controller.navigationItem.leftItemsSupplementBackButton = true
-        controller.title = "Day \(forecast.day)"
+        controller.title = "Day \(day.day)"
       }
     }
   }
+}
+
+
+//MARK: - UITableViewController
+extension MasterViewController {
   
-  // MARK: - Table View
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return forecast.count
+    return dataSource.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    let forecast = forecast[indexPath.row]
+    let forecast = dataSource[indexPath.row]
     
     cell.textLabel?.text = "Day \(forecast.day): \(forecast.description)"
     //TODO: Set textLabel color to gray if image has been downloaded
     
     return cell
+  }
+}
+
+
+//MARK: - ImageDownloadDelegate
+extension MasterViewController: ImageDownloadDelegate {
+  
+  func imageDownloaded(for forecast: Day) {
+    //TODO: Update models and reloadData
   }
 }
