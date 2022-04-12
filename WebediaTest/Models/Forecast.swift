a8ad9c130aac6a5f07d75a8dbae9fb20916e0cc6
Reqven <manuu.marchand@gmail.com>
2022-04-12 02:10:57 +0200

    Add Forecast and Day models

---
+++ WebediaTest/Models/Forecast.swift
@@ -0,0 +1,32 @@
+//
+//  Forecast.swift
+//  WebediaTest
+//
+//  Created by Manu on 12/04/2022.
+//
+
+import Foundation
+
+typealias Forecast = [Day]
+
+struct Day: Codable {
+  let day: String
+  var description: String
+  let sunrise: Int
+  let sunset: Int
+  let chanceRain: Double
+  let high: Int
+  let low: Int
+  let image: String
+  
+  enum CodingKeys: String, CodingKey {
+    case day
+    case description
+    case sunrise
+    case sunset
+    case chanceRain = "chance_rain"
+    case high
+    case low
+    case image
+  }
+}
