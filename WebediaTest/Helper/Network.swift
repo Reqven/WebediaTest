a5a60400c8e78b7b1692638ecad72a5c040fc5f6
Reqven <manuu.marchand@gmail.com>
2022-04-13 14:24:39 +0200

    Add image() method in the Network helper for downloading images

--- WebediaTest/Helper/Network.swift
+++ WebediaTest/Helper/Network.swift
@@ -5,7 +5,7 @@
 //  Created by Manu on 12/04/2022.
 //
 
-import Foundation
+import UIKit
 
 class Network {
   
@@ -20,6 +20,16 @@ class Network {
           } catch let error {
             completion(.failure(error))
           }
+      }
+    }.resume()
+  }
+  
+  static func image(from url: URL, _ completion: @escaping (Result<UIImage, Error>) -> Void) {
+    URLSession.shared.dataTask(with: url) { data, _, error in
+      if let error = error {
+        completion(.failure(error))
+      } else if let data = data, let image = UIImage(data: data) {
+        completion(.success(image))
       }
     }.resume()
   }
