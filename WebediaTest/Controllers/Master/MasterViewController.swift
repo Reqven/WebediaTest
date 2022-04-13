4f6519f5032fec2b5164b9a0b614c48d60409e85
Reqven <manuu.marchand@gmail.com>
2022-04-13 05:15:36 +0200

    Use MVVM architecture for the MasterViewController

---
+++ WebediaTest/Controllers/Master/MasterViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MasterViewController.swift
+//  WebediaTest
+
+import UIKit
+
+protocol MasterViewControllerDelegate {
+  func didSort(with type: ForecastListType)
+}
+
+class MasterViewController: UITableViewController {
+  
+  //MARK: - Properties
+  @IBOutlet weak var sortingControl: UISegmentedControl!
+  private var viewModel = MasterViewControllerViewModel()
+  private var delegate: MasterViewControllerDelegate?
+  
+
+  //MARK: - Methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+  }
+  
+  private func setup() {
+    viewModel.delegate = self
+    viewModel.loadForecast()
+    
+    delegate = viewModel
+    tableView.delegate = viewModel
+    tableView.dataSource = viewModel
+    
+    sortingControl.addTarget(self, action: #selector(self.onSort), for: .valueChanged)
+  }
+  
+  @objc func onSort(_ segmentedControl: UISegmentedControl) {
+    let index = segmentedControl.selectedSegmentIndex
+    guard let type = ForecastListType(rawValue: index) else { return }
+    
+    delegate?.didSort(with: type)
+    tableView.reloadData()
+  }
+}
+
+
+//MARK: - ForecastListDelegate
+extension MasterViewController: ForecastListDelegate {
+  
+  func didLoad() {
+    tableView.reloadData()
+  }
+  
+  func didSelect(forecast: Forecast) {
+    guard let splitViewController = splitViewController as? SplitViewController else { return }
+    guard let rootNavigationController = splitViewController.detailRootController else { return }
+    guard let detailViewController = rootNavigationController.topViewController as? DetailViewController else { return }
+    
+    detailViewController.day = forecast
+    splitViewController.showDetailViewController(rootNavigationController, sender: self)
+  }
+}
+
+
+//MARK: - ImageDownloadDelegate
+extension MasterViewController: ImageDownloadDelegate {
+  
+  func imageDownloaded(for forecast: Forecast) {
+    //TODO: Update models and reloadData
+  }
+}
---
+++ WebediaTest/Controllers/Master/MasterViewControllerViewModel.swift
@@ -0,0 +1,77 @@
+//
+//  MasterViewControllerViewModel.swift
+//  WebediaTest
+//
+//  Created by Manu on 13/04/2022.
+//
+
+import UIKit
+
+protocol ForecastListDelegate {
+  func didLoad()
+  func didSelect(forecast: Forecast)
+}
+
+class MasterViewControllerViewModel: NSObject {
+  
+  //MARK: - Properties
+  var delegate: ForecastListDelegate?
+  
+  private var forecast = ForecastList()
+  private var upcoming: ForecastList { forecast.upcoming() }
+  private var hottest: ForecastList { forecast.hottest() }
+  
+  private var dataSourceType: ForecastListType = .upcoming
+  private var dataSource: ForecastList {
+    switch(dataSourceType) {
+      case .upcoming: return upcoming
+      case .hottest: return hottest
+    }
+  }
+  
+  
+  //MARK: - Methods
+  func loadForecast() {
+    let stringURL = "https://xmfw.github.io/forecast.json"
+    guard let forecastURL = URL(string: stringURL) else { return }
+    
+    Network.json(from: forecastURL, as: ForecastList.self) { result in
+      switch(result) {
+      case .failure(let error): print(error)
+        case .success(let data): DispatchQueue.main.async {
+          self.forecast = data
+          self.delegate?.didLoad()
+        }
+      }
+    }
+  }
+}
+
+
+//MARK: - MasterViewControllerDelegate
+extension MasterViewControllerViewModel: MasterViewControllerDelegate {
+  
+  func didSort(with type: ForecastListType) {
+    dataSourceType = type
+  }
+}
+
+
+//MARK: TableView
+extension MasterViewControllerViewModel: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    delegate?.didSelect(forecast: dataSource[indexPath.row])
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dataSource.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: ForecastCell.reusableIdentifier, for: indexPath) as! ForecastCell
+    cell.configure(with: dataSource[indexPath.row])
+    return cell
+  }
+}
